623339cc05bc1e2cc304931aca3f4f8c16db9671
menglingtong <lingtongmeng@qq.com>
2019-03-27 19:37:40 +0800

    添加test函数

--- day73_git/src/com/itheima/git/FirstClass.java
+++ day73_git/src/com/itheima/git/FirstClass.java
@@ -4,4 +4,9 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("hello git project");
     }
+
+    public void test(){
+        int i = 0;
+        i += 10;
+    }
 }
